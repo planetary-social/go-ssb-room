5e2a2da80877facb1d80a7e8508a82f74799e9d7
boreq <boreq@0x46.net>
2022-12-06 16:09:57 +0100

    Treat web/app and web/assets as one directory
    This commit changes the way the assets are loaded. First we try to load
    assets from web/app and if that fails we load them from web/assets. This
    means that files can be in either one of those directories with web/app
    taking priority.

--- web/embedded_prod.go
+++ web/embedded_prod.go
@@ -2,15 +2,19 @@
 //
 // SPDX-License-Identifier: MIT
 
+//go:build !dev
 // +build !dev
 
 package web
 
 import (
 	"embed"
+	"fmt"
+	"github.com/friendsofgo/errors"
 	"io/fs"
 	"log"
 	"net/http"
+	"os"
 )
 
 // Production can be used to determain different aspects at compile time (like hot template reloading)
@@ -25,7 +29,7 @@ var (
 func init() {
 	var err error
 
-	prefixedAssets, err := fs.Sub(embedAssets, "assets")
+	prefixedAssets, err := newAppFS(embedAssets, embedApp)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,3 +47,39 @@ var embedTemplates embed.FS
 
 //go:embed assets/*
 var embedAssets embed.FS
+
+//go:embed app/*
+var embedApp embed.FS
+
+type appFS struct {
+	app    fs.FS
+	assets fs.FS
+}
+
+func newAppFS(assets embed.FS, app embed.FS) (fs.FS, error) {
+	prefixedApp, err := fs.Sub(app, "app")
+	if err != nil {
+		return nil, err
+	}
+
+	prefixedAssets, err := fs.Sub(assets, "assets")
+	if err != nil {
+		return nil, err
+	}
+
+	return &appFS{
+		app:    prefixedApp,
+		assets: prefixedAssets,
+	}, nil
+}
+
+func (a appFS) Open(name string) (fs.File, error) {
+	fmt.Println("open", name)
+	v, err := a.app.Open(name)
+	if errors.Is(err, os.ErrNotExist) {
+		fmt.Println("returning from assets", name)
+		return a.assets.Open(name)
+	}
+	fmt.Println("returning from app", name)
+	return v, err
+}
